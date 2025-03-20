ee61aa084b5738c928d28c364fdacf9aa4d002b6
Adrian-24914 <pen24914@uvg.edu.gt>
2025-03-20 11:07:37 -0600

    pruebas Unitarias finalizadas

--- LispLexerTest.java
+++ LispLexerTest.java
@@ -11,4 +11,25 @@ public class LispLexerTest {
         Assert.assertTrue(lexer.verificarParentesis("(+ 1 2)"));
         Assert.assertTrue(lexer.verificarParentesis("(- 9 (* 3 5))"));
     }
+
+    @Test
+    public void testVerificarParentesis_ExpresionIncorrecta() {
+        lexer = new LispLexer();
+        Assert.assertFalse(lexer.verificarParentesis("(+ 1 2")); // Falta cierre
+        Assert.assertFalse(lexer.verificarParentesis(")(+ 1 2)(")); // Cierre sin apertura
+    }
+
+    @Test
+    public void testTokenizar_ExpresionSimple() {
+        lexer = new LispLexer();
+        List<String> esperado = Arrays.asList("(", "+", "1", "2", ")");
+        Assert.assertEquals(esperado, lexer.tokenizar("(+ 1 2)"));
+    }
+
+    @Test
+    public void testTokenizar_ExpresionCompuesta() {
+        lexer = new LispLexer();
+        List<String> esperado = Arrays.asList("(", "-", "9", "(", "*", "3", "5", ")", ")");
+        Assert.assertEquals(esperado, lexer.tokenizar("(- 9 (* 3 5))"));
+    }
 }
