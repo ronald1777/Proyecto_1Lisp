7f1595499e43aef3e0baa53468c08c4ed2c652fc
Adrian-24914 <pen24914@uvg.edu.gt>
2025-03-20 20:28:00 -0600

    cambios al tester

--- LispLexerTest.java
+++ LispLexerTest.java
@@ -1,35 +1,20 @@
 import org.junit.*;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class LispLexerTest {
-    private LispLexer lexer;
+    private Lexer lexer;
 
     @Test
     public void testVerificarParentesis_ExpresionCorrecta() {
-        lexer = new LispLexer();
-        Assert.assertTrue(lexer.verificarParentesis("(+ 1 2)"));
-        Assert.assertTrue(lexer.verificarParentesis("(- 9 (* 3 5))"));
+        lexer = new Lexer("(+ 1 2)");
+        Assert.assertTrue(lexer.verificarParentesis());
     }
 
     @Test
     public void testVerificarParentesis_ExpresionIncorrecta() {
-        lexer = new LispLexer();
-        Assert.assertFalse(lexer.verificarParentesis("(+ 1 2")); // Falta cierre
-        Assert.assertFalse(lexer.verificarParentesis(")(+ 1 2)(")); // Cierre sin apertura
+        lexer = new Lexer("(+ 1 2"); // Falta cierre
+        Assert.assertFalse(lexer.verificarParentesis());
     }
 
-    @Test
-    public void testTokenizar_ExpresionSimple() {
-        lexer = new LispLexer();
-        List<String> esperado = Arrays.asList("(", "+", "1", "2", ")");
-        Assert.assertEquals(esperado, lexer.tokenizar("(+ 1 2)"));
-    }
-
-    @Test
-    public void testTokenizar_ExpresionCompuesta() {
-        lexer = new LispLexer();
-        List<String> esperado = Arrays.asList("(", "-", "9", "(", "*", "3", "5", ")", ")");
-        Assert.assertEquals(esperado, lexer.tokenizar("(- 9 (* 3 5))"));
-    }
+    
 }
